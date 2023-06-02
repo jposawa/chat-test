cf237697b8b41cbd5269ec727c023c615a5a3638
JPOsawa <jposawa@gmail.com>
2023-06-02 13:52:03 -0300

    Changed texts position

--- src/components/MessagesList/MessagesList.jsx
+++ src/components/MessagesList/MessagesList.jsx
@@ -3,12 +3,18 @@ import { useRecoilValue } from "recoil";
 import { messagesListState, usernameState } from "../../shared/state";
 import PropTypes from "prop-types";
 
-export const MessagesList = ({ className }) => {
+export const MessagesList = ({
+  className,
+  lineBgColor = "var(--greyColor)",
+}) => {
   const username = useRecoilValue(usernameState);
   const messagesList = useRecoilValue(messagesListState);
 
   return (
-    <section className={`${styles.messagesList} ${className}`}>
+    <section
+      className={`${styles.messagesList} ${className}`}
+      style={{ "--bgLineColor": lineBgColor }}
+    >
       {messagesList.map((message, index) => (
         <p key={`msg${index}`}>
           {index % 2 === 1 && <span />}
@@ -21,4 +27,5 @@ export const MessagesList = ({ className }) => {
 
 MessagesList.propTypes = {
   className: PropTypes.string,
+  lineBgColor: PropTypes.string,
 };
